24aae12fc9524fed48699860218a4615b344edf0
radheWorkfile <raccoder@gmail.com>
2025-03-27 12:38:55 +0530

    added

--- base/base.php
+++ base/base.php
@@ -1,6 +1,9 @@
-    <!-- heading-rotate 1. - aos-init aos-animate" data-aos="flip-up" data-wow-duration="2000" data-aos-easing="ease-out-cubic" -->
-    <!-- test-zoom - aos-init aos-animate" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="2000" -->
-    <!-- img-zoom - aos-init aos-animate" data-aos="zoom-in" data-aos-duration="3000" -->
-    <!-- box-rotate - aos-init aos-animate" data-aos="flip-down" data-aos-duration="3000"> -->
-     <!-- card-flip - aos-init" data-aos="flip-left" data-aos-duration="3000" -->
-     <!-- card-zoom - aos-init" data-aos="zoom-in" data-aos-duration="3000" -->
+<!-- test-zoom - aos-init aos-animate" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="2000" -->
+<!-- card-zoom - aos-init" data-aos="zoom-in" data-aos-duration="3000" -->
+<!-- img-zoom - aos-init aos-animate" data-aos="zoom-in" data-aos-duration="3000" -->
+<!-- heading-rotate 1. - aos-init aos-animate" data-aos="flip-up" data-wow-duration="2000" data-aos-easing="ease-out-cubic" -->
+<!-- box-rotate - aos-init aos-animate" data-aos="flip-down" data-aos-duration="3000"> -->
+<!-- image-rotate <img  data-aos="zoom-in" data-animate-duration="2000" class="aos-init aos-animate"> -->
+<!-- totate-text :  class="aos-init aos-animate" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000"> -->
+<!-- card-flip - aos-init" data-aos="flip-left" data-aos-duration="3000" -->
+
