9cf77acea922f5ed34b4f72f9906150df00c89ad
radheWorkfile <raccoder@gmail.com>
2025-04-01 10:10:25 +0530

    added some file

---
+++ include/css.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="css/animate.css" type="text/css" media="all" />
+<link rel="icon" href="images/logo.png" sizes="64x64">
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link href="css/css_slider.css" type="text/css" rel="stylesheet" media="all">
+	<!-- banner slider -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+ 
+
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+
+
+	<style>
+/* General Styles */
+.text-center {
+    text-align: center;
+}
+
+.text-shadow {
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.highlight-address {
+    background-color: #ff5400;
+    border-radius: 4rem;
+    padding: 0.5rem 1rem;
+    font-weight: 600;
+    line-height: 20px;
+    margin-top: 5rem;
+    color: #fff; /* For better contrast on orange background */
+}
+
+.suse-font {
+    font-family: "SUSE", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 600;
+}
+
+
+		@media screen and (max-width: 600px) {
+            .textMan {
+                display: none;
+            }
+        }
+
+        @media screen and (min-width: 601px) and (max-width: 768px) {
+            .textMan {
+                display: none;
+            }
+        }
+
+        @media screen and (min-width: 769px) and (max-width: 992px){
+            .textMan {
+                display: none;
+            }
+        }
+
+	</style>
---
+++ include/default.php
@@ -0,0 +1,18 @@
+<?php 
+ $company = 'ARYAMAN HIGHER EDUCATION';
+ $company_1 = 'Aryaman Higher Education';
+ $mobile_1 = '9308819517';
+ $mobile_2 = '9430679729';
+ $email = 'mrfoudation392@gmail.com';
+ $address = "Arvind Marg, Lachchu Bihga, Nagarnausa, Nalanda - 801305";
+ $facebook = 'www.facebook.com';
+ $instagram = 'www.instagram.com';
+ $teligram = 'www.teligram.com';
+ $youtube = 'www.youtube.com';
+ $twitter = 'www.twitter.com';
+
+?>
+<!-- <h1 class="card-title caption-line" id="typing-text"></h1> -->
+
+
+
