98000db0d8bca3d2c3774f738d3c4f2d0fab06ae
radheWorkfile <raccoder@gmail.com>
2025-04-16 16:06:02 +0530

    added some file to update.

--- include/css.php
+++ include/css.php
@@ -11,27 +11,11 @@
 
     <link rel="shortcut icon" href="<?php echo $favicon;?>" type="image/x-icon">
 
-    <!-- ***************************************************************** -->
-
     <style>
         p{
             color:black;
         }
     </style>
-
-    
-    <!-- // text-anim   text-anim2   #preloader    sticky-header    tg-swiper__slider  masonary-active  
-        popup-image    counter-number     progress-bar    popup-video    wowAnimation    aosAnimation   -->
-
-    <!-- h - roteate ( class="aos-init aos-animate" data-aos="flip-up" data-aos-duration="2000" ) -->
-    <!-- use in pragraph ->  data-aos="fade-up" data-aos-duration="3000" -->
-    <!-- use in Headming ->  text-anim   text-anim2   -->
-    <!-- Number count -> .counter-number    -->
-    <!-- .box-icon ( keep style.css for rotate icon )    -->
-    <!-- p tag of aos => data-aos="zoom-in" data-aos-duration="3000"  -->
-    <!-- p tag  of aos => data-aos="fade-up" data-aos-duration="3000"  -->
-    <!-- image -> .image-anim (for css shadow)   .jump (image jump)   .movingX ( movieing images ) -->
-
 
     <link rel="stylesheet" href="assets/media/css/animate.min.css">
     <link rel="stylesheet" href="assets/media/css/magnific-popup.css">
