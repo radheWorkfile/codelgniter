5a09cf55fae5967fcf0d8d760e4184b05042b7e1
radheWorkfile <raccoder@gmail.com>
2025-06-03 12:52:09 +0530

    add card.

---
+++ view/card.php
@@ -0,0 +1,21 @@
+   <!-- Account Details section start -->
+                     <div class="col-md-4">
+                    <div class=""style="border:1px solid white;border-radius:0.5rem;padding:3rem 1rem;">
+                        <div class=""style="margin:auto;">
+                        <img src="https://www.researchgate.net/profile/Vinod-Shukla/publication/340398294/figure/fig3/AS:876175308103681@1585907880294/Sample-Figure-of-QR-Code-QR-code-works-in-a-simple-way-It-all-starts-of-by-feeding-in.ppm" style="height:6rem;" alt="">
+                     </div>
+                        <h3 class="text-whitte">Dues Amount&nbsp;:&nbsp;₹840000</h3>
+                        <div>
+                        <p class="text-whitte text-manage" > <span>Account Holder :</span> <span>John Doe</span> </p>
+                        <p class="text-whitte text-manage" > <span>Account Number :</span> <span>424234324242343234</span> </p>
+                        <p class="text-whitte text-manage" > <span>Bank Branch :</span> <span>XYZ Branch</span> </p>
+                        <p class="text-whitte text-manage" > <span>Bank Name :</span> <span>ABC0123456</span> </p>
+                        <p class="text-whitte text-manage" > <span>Bank IFSC :</span> <span>ABC0123456</span> </p>
+                        <p class="text-whitte text-manage" > <span>UPI Address :</span> <span>2423423423424234</span> </p>
+                        </div>
+                    </div>
+                  </div>
+                    <!-- Account Details section end -->
+<style>
+    .text-manage{ display: flex; justify-content: space-between; }
+</style>
