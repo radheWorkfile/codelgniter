9261abcb44d42013e0566a07b0c6c3ea7ac92aba
radheWorkfile <raccoder@gmail.com>
2025-07-25 18:13:08 +0530

    added some function only.

--- software/0_function.php/add-data/add-data.php
+++ software/0_function.php/add-data/add-data.php
@@ -137,3 +137,69 @@
         echo json_encode($data);
     }
 ?>
+
+<?php
+ public function add_kanyadan_ben()
+{
+    $this->form_validation->set_rules('br_name', 'Bride Name', 'trim|required');
+
+    if ($this->form_validation->run() === false) {
+        echo json_encode(array('status' => 'error', 'message' => $this->form_validation->error_array() ));
+        return;
+    }
+
+    $post = $this->input->post();
+
+    $lastId = $this->db->select('id, br_id')->from('kanyadan_youjna_support')->order_by('id', 'DESC')->limit(1)->get()->row();
+
+    if (!empty($lastId) && !empty($lastId->br_id) && $lastId->br_id !== '0') {
+        $lastNumber = (int)substr($lastId->br_id, 3);
+        $lastKHY = 'KHY' . str_pad($lastNumber + 1, 4, '0', STR_PAD_LEFT);
+    } else {
+        $lastKHY = 'KHY1001';
+    }
+
+        $br_image_img         = $this->upload_image('youjna_doc', 'br_image');
+
+        $br_image_img         = ($br_image_img['icon'] === 'success') ? $br_image_img['text'] : '';
+
+        $value = array(
+            
+            'br_id'                  => $lastKHY,
+            'br_image'               => $br_image_img,
+            
+            'br_name'                => $post['br_name'],
+            'date'                   => $post['marrageDate'],
+        );
+
+        $save = $this->db->insert('kanyadan_youjna_support', $value);
+
+    if ($save) {
+        $data = array('status' => 'success','message' => 'Thank you! Data added successfully.');
+    } else {
+        $data = array('status' => 'error','message' => 'Something went wrong. Please try again later.');
+    }
+    echo json_encode($data);
+}
+
+    function update_image()
+    {
+        $mem_id = $this->input->post('mem_id');
+        $da = $this->upload_image('member', 'image');
+
+        if ($da['icon'] == 'success') {
+            $img = $da['text'];
+            $data = array(
+                'my_img' => $img,
+            );
+            $memb = $this->db->select('my_img')->where('id', $mem_id)->get('member')->row();
+            unlink($memb->my_img);
+            $this->db->where('id', $mem_id)->update('member', $data);
+            $data = array('text' => "<p style='padding:10px;background:green;color:white'>Successfully Updated Image!<p>", "icon" => "success");
+        } else {
+            $data = array('text' => "<p style='padding:10px;border:1px solid red;color:white'>" . $da['text'] . "</p>", "icon" => "error");
+        }
+
+        echo json_encode($data);
+    }
+?>
